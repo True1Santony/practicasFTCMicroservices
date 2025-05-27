4212cca4911921b254ac29b11b8b00f5e6f6686d
Espartaco Nassekine Local <espartaconassekine@gmail.com>
2025-05-27 15:11:20 +0200

    first commit

---
+++ src/main/java/com/tratsformtomicroservices/concessionaire_microservice/ConcessionaireMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tratsformtomicroservices.concessionaire_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionaireMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcessionaireMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tratsformtomicroservices/concessionaire_microservice/ConcessionaireMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tratsformtomicroservices.concessionaire_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionaireMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
